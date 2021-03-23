f7c82d6925d58e0e03f6b4b9ed5baaf279b7ff1d
Eric Sullinger <eric.sullinger1@gmail.com>
2021-03-23 13:20:27 -0400

    selectors added for lists & inputs

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 // SELECTING ELEMENTS//
-//budget display elements --------
+//BUDGET DISPLAY ELEMENTS --------
 const balanceEl = document.querySelector(".balance .value");
 const incomeTotalEl = document.querySelector(".outcome-total");
 const outcomeTotalEl = document.querySelector(".income-total");
-//budget dashboard elements -------
+//BUDGET DASHBOARD ELEMENTS -------
 //dashboard buttons
 const expenseBtn = document.querySelector(".tab1");
 const incomeBtn = document.querySelector(".tab2");
 const allBtn = document.querySelector(".tab3");
-//amnt lists
+//selects div wraps for lists
 const expenseEl = document.querySelector("#expense");
 const incomeEl = document.querySelector("#income");
 const allEl = document.querySelector("#all");
+//selects lists themselves
+const incomeList = document.querySelector("#income .list");
+const expenseList = document.querySelector("#expense .list");
+const allList = document.querySelector("#all .list");
+//selects inputs
+//income
+const addIncome = document.querySelector(".add-income");
+const incomeTitle = document.getElementById("income-title-input");
+const incomeAmount = document.getElementById("income-amount-input");
+//expense
+const addExpense= document.querySelector(".add-expense");
+const expenseTitle = document.getElementById("expense-title-input");
+const expenseAmount = document.getElementById("expense-amount-input");
