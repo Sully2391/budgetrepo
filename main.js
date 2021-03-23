91b4f8f95392a5a376777fbec57a3ba620d83b39
Eric Sullinger <eric.sullinger1@gmail.com>
2021-03-23 13:07:19 -0400

    more element selectors added for dashboard

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-// Selecting elements //
-//budget display elements
+// SELECTING ELEMENTS//
+//budget display elements --------
 const balanceEl = document.querySelector(".balance .value");
 const incomeTotalEl = document.querySelector(".outcome-total");
-const outcomeTotalEl = document.querySelector(".income-total")
+const outcomeTotalEl = document.querySelector(".income-total");
+//budget dashboard elements -------
+//dashboard buttons
+const expenseBtn = document.querySelector(".tab1");
+const incomeBtn = document.querySelector(".tab2");
+const allBtn = document.querySelector(".tab3");
+//amnt lists
+const expenseEl = document.querySelector("#expense");
+const incomeEl = document.querySelector("#income");
+const allEl = document.querySelector("#all");
