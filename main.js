c91a5919197328a2ec0fccc15abfad3906c83a06
Eric Sullinger <eric.sullinger1@gmail.com>
2021-03-23 12:58:15 -0400

    beginning of js file, starting w/ element selectors

---
+++ main.js
@@ -0,0 +1,5 @@
+// Selecting elements //
+//budget display elements
+const balanceEl = document.querySelector(".balance .value");
+const incomeTotalEl = document.querySelector(".outcome-total");
+const outcomeTotalEl = document.querySelector(".income-total")
