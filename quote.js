a81b28f8ae64defed7e5fab6c036798385d6819f
Eric Sullinger <eric.sullinger1@gmail.com>
2021-03-28 14:20:18 -0400

    pulling quotes from one API, may look into a different api later

--- quote.js
+++ quote.js
@@ -2,6 +2,17 @@ const quoteBtn = document.querySelector('#quotegen');
 
 quoteBtn.addEventListener('click', getQuote);
 
-function getQuote() {
-    console.log("a wild quote appears!")
+const url = "https://api.quotable.io/random";
+
+function getQuote(){
+    fetch(url)
+    .then(function(data){
+        return data.json();
+    })
+    .then(function(data){
+        document.getElementById("view-quote").innerHTML = data.content; document.querySelector("#author").innerHTML = "- " + data.author;
+    })
+    .catch(function(err){
+        console.log(err);
+    });
 }
